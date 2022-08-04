5ff49c9b1eeea4c69568b86b1f1544327e70b587
MGASALUCAS <maulidiadam1234@gmail.com>
2022-08-04 06:42:12 +0300

    second commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn streamProject.wsgi
--- streamProject/settings.py
+++ streamProject/settings.py
@@ -12,10 +12,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -27,7 +27,6 @@ SECRET_KEY = 'django-insecure-8u^9m9w2xzx%z!96ctyn)0ut*mo=s*ww8ye8m5x=q-b9vofgvi
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -72,7 +71,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'streamProject.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
@@ -82,7 +80,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -102,7 +99,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
@@ -114,18 +110,19 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
 
-
-MEDIA_ROOT = os.path.join(BASE_DIR,'streamApp/images/test/')  # This provide a root directory to where should we save and store our media  files
+MEDIA_ROOT = os.path.join(BASE_DIR,
+                          'streamApp/images/test/')  # This provide a root directory to where should we save and store our media  files
 MEDIA_URL = '/streamApp/images/test/'
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Activate Django - heroku
+django_heroku.settings(locals())
